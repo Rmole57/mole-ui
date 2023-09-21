1b6ed66ef6936ee1f127f87054ac0c8788e02ff5
Rick <35900587+Rmole57@users.noreply.github.com>
2023-09-20 22:26:54 -0400

    initial library setup; add generic Button component template for testing

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.ts',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader',
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    static: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 4000,
+  },
+  plugins: [new ForkTsCheckerWebpackPlugin()],
+  watchOptions: {
+    ignored: /node_modules/,
+  },
+};
